063eba2ace0686fb38422b1db4125706a5a3eada
Motko <77466238+Motko222@users.noreply.github.com>
2025-04-15 10:36:54 +0200

    Update report.sh

--- report.sh
+++ report.sh
@@ -9,6 +9,8 @@ source $path/env
 version=$()
 service=$(sudo systemctl status $folder --no-pager | grep "active (running)" | wc -l)
 errors=$(journalctl -u $folder.service --since "1 hour ago" --no-hostname -o cat | grep -c -E "rror|ERR")
+rate-hour=$(journalctl -u $folder.service --since "1 hour ago" --no-hostname -o cat | grep -E "Confirmed" | awk '{print $6}' | sed 's/\./,/g' | awk '{sum+=$1} END {print sum}')
+rate-day=$(journalctl -u $folder.service --since "1 day ago" --no-hostname -o cat | grep -E "Confirmed" | awk '{print $6}' | sed 's/\./,/g' | awk '{sum+=$1} END {print sum}')
 
 status="ok" && message=""
 [ $errors -gt 500 ] && status="warning" && message="errors=$errors";
@@ -34,7 +36,8 @@ cat >$json << EOF
         "service":$service,
         "errors":$errors,
         "url":"",
-        "balance":"$balance"
+        "rate-hour":"$rate-hour",
+        "rate-day":"$rate-day"
   }
 }
 EOF

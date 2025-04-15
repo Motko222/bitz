cf140bd60230257df60a3e47cd50bf229481ee41
Motko <77466238+Motko222@users.noreply.github.com>
2025-04-15 11:47:08 +0200

    Update report.sh

--- report.sh
+++ report.sh
@@ -6,13 +6,16 @@ json=/root/logs/report-$folder
 source /root/.bash_profile
 source $path/env
 
-version=$()
+version=$(bitz -V | awk '{print $NF}')
 service=$(sudo systemctl status $folder --no-pager | grep "active (running)" | wc -l)
 errors=$(journalctl -u $folder.service --since "1 hour ago" --no-hostname -o cat | grep -c -E "rror|ERR")
-rate_hour=$(journalctl -u $folder.service --since "1 hour ago" --no-hostname -o cat | grep -E "Confirmed" | awk '{print $6}' | sed 's/\./,/g' | awk '{sum+=$1} END {printf "%0.2f",sum}')
-rate_day=$(journalctl -u $folder.service --since "1 day ago" --no-hostname -o cat | grep -E "Confirmed" | awk '{print $6}' | sed 's/\./,/g' | awk '{sum+=$1} END {printf "%0.2f",sum}')
+#rate_hour=$(journalctl -u $folder.service --since "1 hour ago" --no-hostname -o cat | grep -E "Confirmed" | awk '{print $6}' | sed 's/\./,/g' | awk '{sum+=$1} END {printf "%0.2f",sum}')
+#rate_day=$(journalctl -u $folder.service --since "1 day ago" --no-hostname -o cat | grep -E "Confirmed" | awk '{print $6}' | sed 's/\./,/g' | awk '{sum+=$1} END {printf "%0.2f",sum}')
+bitz account >/root/logs/bitz-account
 
-status="ok" && message="rate $rate_hour/$rate_day"
+miner_balance=$(cat /root/logs/bitz-account | grep Balance | tail -1 | awk '{print $2}')
+
+status="ok" && message=""
 [ $errors -gt 500 ] && status="warning" && message="errors=$errors";
 [ $service -ne 1 ] && status="error" && message="service not running";
 
@@ -35,8 +38,7 @@ cat >$json << EOF
         "service":$service,
         "errors":$errors,
         "url":"",
-        "rate_hour":"$rate_hour",
-        "rate_day":"$rate_day"
+        "miner_balance":"$miner_balance"
   }
 }
 EOF

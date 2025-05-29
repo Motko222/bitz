85f534a64599ceff9198a984336fb02c8ed493b1
Motko <77466238+Motko222@users.noreply.github.com>
2025-05-29 10:47:56 +0200

    Update report.sh

--- report.sh
+++ report.sh
@@ -1,3 +1,4 @@
+  GNU nano 6.2                                                                                                                                     /root/scripts/bitz/report.sh                                                                                                                                              
 #!/bin/bash
 
 path=$(cd -- $(dirname -- "${BASH_SOURCE[0]}") && pwd)
@@ -12,10 +13,13 @@ errors=$(journalctl -u $folder.service --since "1 hour ago" --no-hostname -o cat
 #rate_hour=$(journalctl -u $folder.service --since "1 hour ago" --no-hostname -o cat | grep -E "Confirmed" | awk '{print $6}' | sed 's/\./,/g' | awk '{sum+=$1} END {printf "%0.2f",sum}')
 #rate_day=$(journalctl -u $folder.service --since "1 day ago" --no-hostname -o cat | grep -E "Confirmed" | awk '{print $6}' | sed 's/\./,/g' | awk '{sum+=$1} END {printf "%0.2f",sum}')
 bitz account >/root/logs/bitz-account
+echo n | bitz claim -p $POOL >>/root/logs/bitz-account
 
-miner_balance=$(cat /root/logs/bitz-account | grep Balance | tail -1 | awk '{print $2}')
 
-status="ok" && message="bal $miner_balance"
+miner_balance=$(cat /root/logs/bitz-account | grep "Balance" | tail -1 | awk '{print $2}')
+pool_balance=$(cat /root/logs/bitz-account | grep "You are about to claim" | tail -1 | awk '{print $6}')
+
+status="ok" && message="bal $pool_balance"
 [ $errors -gt 500 ] && status="warning" && message="errors=$errors";
 [ $service -ne 1 ] && status="error" && message="service not running";
 
@@ -35,10 +39,11 @@ cat >$json << EOF
         "version":"$version",
         "status":"$status",
         "message":"$message",
-        "service":$service,
-        "errors":$errors,
+        "service":"$service",
+        "errors":"$errors",
         "url":"",
-        "miner_balance":"$miner_balance"
+        "score1":"$pool_balance (pool)",
+        "score2":"$miner_balance (miner)"
   }
 }
 EOF

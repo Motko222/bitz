c83046bdf6fd08749c72f9442c639bcf1da00a72
Motko <77466238+Motko222@users.noreply.github.com>
2025-06-05 08:21:49 +0200

    Update claim.sh

--- claim.sh
+++ claim.sh
@@ -4,4 +4,5 @@ path=$(cd -- $(dirname -- "${BASH_SOURCE[0]}") && pwd)
 folder=$(echo $path | awk -F/ '{print $NF}')
 source $path/env
 
-[ -z $1 ] && bitz claim -p $POOL || echo y | bitz claim -p $POOL
+cd /root/.cargo/bin/bitz
+[ -z $1 ] && ./bitz claim -p $POOL || echo y | ./bitz claim -p $POOL
